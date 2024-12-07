cea9ef22593ef3aada043e16a9599d5668afa53c
glizzygobblrr <S10257120@connect.np.edu.sg>
2024-12-07 13:54:40 +0800

    add logo to navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/Header.css';
+import logo from '../assets/PDBaker.png';
 
 const Header = () => {
   const scrollToSection = (event, sectionId) => {
@@ -18,12 +19,15 @@ const Header = () => {
     <header>
       <h1 data-text="Hey, I'm Daniel!">Hey, I'm Daniel!</h1>
       <nav>
-        <ul>
-          <li><a href="#about" onClick={(e) => scrollToSection(e, 'about')}>About Me</a></li>
-          <li><a href="#projects" onClick={(e) => scrollToSection(e, 'projects')}>Projects & Events</a></li>
-          <li><a href="#certs" onClick={(e) => scrollToSection(e, 'certs')}>Proficiencies</a></li>
-          <li><a href="#contact" onClick={(e) => scrollToSection(e, 'contact')}>Contacts</a></li>
-        </ul>
+        <div className="nav-container">
+          <img src={logo} alt="Logo" className="logo" />
+          <ul>
+            <li><a href="#about" onClick={(e) => scrollToSection(e, 'about')}>About Me</a></li>
+            <li><a href="#projects" onClick={(e) => scrollToSection(e, 'projects')}>Projects & Events</a></li>
+            <li><a href="#certs" onClick={(e) => scrollToSection(e, 'certs')}>Proficiencies</a></li>
+            <li><a href="#contact" onClick={(e) => scrollToSection(e, 'contact')}>Contacts</a></li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
